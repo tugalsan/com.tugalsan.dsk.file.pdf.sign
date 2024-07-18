3be2d8f0b6135f4ede7cfb29d9d84a03cfbc2ace
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-18 07:22:05 +0300

    Update run_sign_with_props.bat

--- run_sign_with_props.bat
+++ run_sign_with_props.bat
@@ -1,2 +1,2 @@
 del file_signed.pdf
-java -jar target\com.tugalsan.dsk.pdf.sign-1.0-SNAPSHOT-jar-with-dependencies.jar --load-properties-file settings.properties
+java -jar target\com.tugalsan.dsk.pdf.sign-1.0-SNAPSHOT-jar-with-dependencies.jar --load-properties-file config.properties
